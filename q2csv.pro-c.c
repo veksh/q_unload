c2169f0ab0a7f37390fbb4957e5e93a2ea529e1a
Alexey Vekshin <alex@maxidom.ru>
2016-06-02 18:32:25 +0300

    removed some debug, fixed indent

--- q2csv.pro-c.c
+++ q2csv.pro-c.c
@@ -119,22 +119,15 @@ static char * read_file(char * filepath)
   FILE *fh = fopen(filepath, "rb");
   if ( fh != NULL )
   {
-    fseek(fh, 0L, SEEK_END);
-    long s = ftell(fh);
-    rewind(fh);
-    buffer = malloc(s);
-    if ( buffer != NULL )
-    {
-      fread(buffer, s, 1, fh);
-      // we can now close the file
-      fclose(fh); fh = NULL;
- 
-      // do something, e.g.
-      fwrite(buffer, s, 1, stdout);
- 
-      // free(buffer);
-    }
-    if (fh != NULL) fclose(fh);
+      fseek(fh, 0L, SEEK_END);
+      long fs = ftell(fh);
+      rewind(fh);
+      buffer = malloc(fs);
+      if ( buffer != NULL ) 
+      {
+          fread(buffer, fs, 1, fh);
+      }
+      fclose(fh);
   }
   return buffer;
 }
